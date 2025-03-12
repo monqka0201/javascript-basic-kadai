d72c465c6e8e212f2a3b69774b8e4571f84cb139
monqka0201 <sca_mqm1@yahoo.co.jp>
2025-03-12 19:31:46 +0900

    7章課題の再提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,6 +1,10 @@
-let num = 4;
+let num = 5;
 
-if (num % 3 == 0) {
+if ((num % 3 == 0) && (num % 5 == 0)) {
+  console.log('3と5の倍数です')
+}
+
+else if (num % 3 == 0) {
   console.log('3の倍数です')
 }
 else if (num % 5 == 0){
